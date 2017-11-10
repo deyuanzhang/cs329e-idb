291fdf5f883225e280aaf746e49435d05d032329
freewilly95 <williamnguyen195@gmail.com>
2017-11-10 13:55:29 -0600

    models

--- models.py
+++ models.py
@@ -1,6 +1,3 @@
-'''
-This file defines the models for a book
-'''
 # for manipulating diff parts of Python's run-time environment
 import sys
 import os
@@ -16,6 +13,11 @@ from sqlalchemy.orm import relationship
 # for configuring code at the end of the file
 from sqlalchemy import create_engine
 
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 # for creating an instance of the declarative_base class
 # (the declarative base class will let SQLAlchemy know 
 # that our classes are special SQLAlchemy classes that 
@@ -23,40 +25,14 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Pokemon(Base):
-	__tablename__ = 'pokemon'	
-	name = Column(String(80), nullable = False, primary_key = True)
-	moves = Column(String(80), foreign_key = True)
-    pokemon_types = Column(String(80), foreign_key = True)
-    attack = Column(Integer, nullable = false)
-    defense = Column(Integer, nullable = false)
-    sp_attack = Column(Integer, nullable = false)
-    sp_defense = Column(Integer, nullable = false)
+	__tablename__ = 'pokemon'
+	
+	title = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
 
-class Move(Base):
-	__tablename__ = 'moves'	
-	name = Column(String(80), nullable = False, primary_key = True)
-    power = Column(Integer, nullable = False)
-    accuracy = Column(Integer, nullable = False)
-    move_type = Column(String(80), nullable = False)
-    pp = Column(Integer, nullable = False)
-
-class Types(Base):
-	__tablename__ = 'types'	
-	name = Column(String(80), nullable = False, primary_key = True)
-	name = Column(String(80), nullable = False, primary_key = True)
-    name = Column(String(80), nullable = False, primary_key = True)
-    name = Column(String(80), nullable = False, primary_key = True)
-
-SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:asd123@localhost/bookdb')
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:group8books@localhost/bookdb')
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
 
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
-
-# try to run this file:
-# python create_db.py
-# you may receive the following error:
-# ImportError: No module named 'psycopg2'
-# This indicates that you need to install 'psycopg2' module
-# To install the 'psycopg2' module:
-# pip install psycopg2
